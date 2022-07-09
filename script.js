7ff4dadbac927127c100449e04f9ebd12fe8b6f2
Victor Mendes <victorutdsim7@gmail.com>
2022-07-09 20:53:24 -0300

    requisito 16

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const botaoCriaCarta = document.getElementById('criar-carta');
 
+function pegarAleatorio(lista) {
+  return lista[Math.floor(Math.random() * lista.length)];
+}
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const grupoTamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+
 // eslint-disable-next-line max-lines-per-function
 function criaCarta() {
   const textoCarta = document.getElementById('carta-texto').value.trim();
@@ -11,6 +19,9 @@ function criaCarta() {
     const arrayPalavras = textoCarta.split(' ');
     for (let index = 0; index < arrayPalavras.length; index += 1) {
       const spanPalavra = document.createElement('span');
+      spanPalavra.style.display = 'inline-block';
+      spanPalavra.classList.add(pegarAleatorio(grupoEstilo), pegarAleatorio(grupoTamanho));
+      spanPalavra.classList.add(pegarAleatorio(rotacao), pegarAleatorio(inclinacao));
       spanPalavra.innerText = `${arrayPalavras[index]}`;
       cartaGerada.appendChild(spanPalavra);
     }
